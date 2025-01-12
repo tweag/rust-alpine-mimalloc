70de3878b542eeb59f7fadf9502eb27a2a082b62
Cheng Shao <terrorjack@type.dance>
2025-01-12 14:28:41 +0000

    Set up default cargo config.toml
    - Default to 8M default stack size, musl's 128K default is too small
    - Default to +crt-static

--- test.sh
+++ test.sh
@@ -4,4 +4,4 @@ set -eu
 
 TARGET=$(rustc -vV | sed -n "s|host: ||p")
 
-RUSTFLAGS="-C target-feature=+crt-static" exec cargo install --root "$PWD" --target "$TARGET" names
+exec cargo install --root "$PWD" --target "$TARGET" names
